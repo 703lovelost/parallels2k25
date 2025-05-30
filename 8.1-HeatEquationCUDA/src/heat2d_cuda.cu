f124cd0f840960f56298217fc7e03379dd70e8fa
703lovelost <dolphinhunter@inbox.ru>
2025-05-30 05:29:47 +0000

    Minor changes.

--- 8.1-HeatEquationCUDA/src/heat2d_cuda.cu
+++ 8.1-HeatEquationCUDA/src/heat2d_cuda.cu
@@ -44,31 +44,45 @@ int main(int argc, char** argv){
     Options opt = parseOptions(argc, argv);
     int N = opt.N;
     int NM = N*N;
+
     double* hA    = (double*)malloc(NM*sizeof(double));
     double* hAnew = (double*)malloc(NM*sizeof(double));
+
     initBoundary(hA, N);
     initBoundary(hAnew, N);
-    for(int j=1;j<N-1;++j) for(int i=1;i<N-1;++i){ hA[j*N+i]=0.0; hAnew[j*N+i]=0.0; }
+
+    for (int j = 1; j < N-1; ++j)
+        for (int i = 1; i < N-1; ++i) {
+            hA[j * N + i] = 0.0;
+            hAnew[j * N + i] = 0.0;
+        }
+
     double *dA, *dAnew, *dDiff, *dMaxErr;
+
     cudaMalloc(&dA,    NM*sizeof(double));
     cudaMalloc(&dAnew, NM*sizeof(double));
     cudaMalloc(&dDiff, NM*sizeof(double));
     cudaMalloc(&dMaxErr, sizeof(double));
     cudaMemcpy(dA,    hA,    NM*sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(dAnew, hAnew, NM*sizeof(double), cudaMemcpyHostToDevice);
+
     void* dTempStorage = nullptr;
     size_t tempStorageBytes = 0;
+
     cub::DeviceReduce::Max(dTempStorage, tempStorageBytes, dDiff, dMaxErr, NM);
     cudaMalloc(&dTempStorage, tempStorageBytes);
+
     int tx=16, ty=16;
     dim3 threads(tx, ty);
     dim3 blocks((N+tx-1)/tx, (N+ty-1)/ty);
+
     double* hMaxErrHost;
     cudaMallocHost(&hMaxErrHost, sizeof(double));
+
     long iter = 0;
     double maxErr = 1e9;
     auto t_start = std::chrono::high_resolution_clock::now();
-    while(maxErr > opt.tol && iter < opt.maxIter){
+    while (maxErr > opt.tol && iter < opt.maxIter){
         ++iter;
         jacobiKernel<<<blocks, threads>>>(dA, dAnew, dDiff, N);
         cub::DeviceReduce::Max(dTempStorage, tempStorageBytes, dDiff, dMaxErr, NM);
@@ -81,18 +95,20 @@ int main(int argc, char** argv){
     auto t_end = std::chrono::high_resolution_clock::now();
     printSummary(iter, maxErr);
     std::chrono::duration<double> elapsed = t_end - t_start;
-    std::cout<<"Elapsed time: "<<elapsed.count()<<" sec\n";
-    if(N == 10){
+    std::cout << "Elapsed time: " << elapsed.count() << " sec\n";
+    
+    if (N == 10) {
         cudaMemcpy(hA, dA, NM*sizeof(double), cudaMemcpyDeviceToHost);
         std::ofstream fout("matrix_10x10.csv");
-        for(int j=0;j<N;++j){
-            for(int i=0;i<N;++i){
-                fout<<hA[j*N+i]<<(i<N-1?",":"");
+        for (int j = 0; j < N; ++j){
+            for (int i = 0; i < N; ++i){
+                fout << hA[j * N + i] << (i < N-1 ? "," : "");
             }
-            fout<<"\n";
+            fout << "\n";
         }
-        std::cout<<"Matrix saved to matrix_10x10.csv\n";
+        std::cout << "Matrix saved to matrix_10x10.csv\n";
     }
+
     cudaFree(dA);
     cudaFree(dAnew);
     cudaFree(dDiff);
@@ -101,5 +117,6 @@ int main(int argc, char** argv){
     cudaFreeHost(hMaxErrHost);
     free(hA);
     free(hAnew);
+    
     return 0;
 }
